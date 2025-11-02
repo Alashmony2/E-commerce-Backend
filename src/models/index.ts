a7e9c9d3dd9ecce6b7b565e1cedfd2abf825695a
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-11-02 23:28:58 +0200

    create admin model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,5 @@
 export * from './common/user.schema';
 export * from './seller/seller.repository';
 export * from './seller/seller.schema';
+export * from './admin/admin.repository';
+export * from './admin/admin.schema';
